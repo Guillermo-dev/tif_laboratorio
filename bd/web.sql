30f4f8373fbe3e2b3694cd2cfd8c81cfb0758498
GuillermoTejera <guilletejera@gmail.com>
2021-11-01 20:03:58 -0300

    arreglo de bds

--- bd/web.sql
+++ bd/web.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS laboratorioWeb;
+/*CREATE DATABASE IF NOT EXISTS laboratorioWeb;*/
 
-CREATE TABLE clientes (
-  cliente_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY,
+CREATE TABLE IF NOT EXISTS clientes (
+  cliente_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   cuil_cuit VARCHAR(25) NOT NULL UNIQUE,
   razon_social VARCHAR(25) NOT NULL,
   nombre VARCHAR(25) NOT NULL,
@@ -10,25 +10,25 @@ CREATE TABLE clientes (
   email VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE campanias (
-  campania_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY,
+CREATE TABLE IF NOT EXISTS campanias (
+  campania_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(25) NOT NULL UNIQUE,
   texto_SMS TEXT NOT NULL,
   cantidad_mensajes INT NOT NULL,
   estado VARCHAR(25) NOT NULL,
   fecha_inicio DATE NOT NULL,
-  cliente_id INT NOT NULL,
+  cliente_id INT UNSIGNED NOT NULL,
   FOREIGN KEY (cliente_id) REFERENCES clientes (cliente_id)
 );
 
-CREATE TABLE localidades(
-  localidad_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY,
+CREATE TABLE IF NOT EXISTS localidades(
+  localidad_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE campanias_localidades (
-  campanias_id INT NOT NULL,
-  localidad_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS campanias_localidades (
+  campania_id INT UNSIGNED NOT NULL,
+  localidad_id INT UNSIGNED NOT NULL,
   PRIMARY KEY (campania_id, localidad_id),
   FOREIGN KEY (campania_id) REFERENCES campanias(campania_id),
   FOREIGN KEY (localidad_id) REFERENCES localidades(localidad_id)
