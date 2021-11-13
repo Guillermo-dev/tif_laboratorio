568b7d29fbc79d82e5f7bb17a8a7367acbb3b5e6
Ivo <ivo1214@live.com>
2021-11-13 17:53:19 -0300

    Insert de localidades, codigos de area y prefijo nacional

---
+++ bd/localidades y codigosArea.sql
@@ -0,0 +1,46 @@
+/*Se crean las localidades*/
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Buenos Aires','Baradero');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Buenos Aires','Campana');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Buenos Aires','Chacabuco');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Buenos Aires','Dolores');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Buenos Aires','Escobar');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Buenos Aires','Zárate');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Santa Fe','Ceres');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Santa Fe','Esperanza');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Santa Fe','Florencia');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Santa Fe','Totoras');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','La Pampa','Abramo');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','La Pampa','Caleufú');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','La Pampa','Ceballos');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Entre Ríos','Aldea Brasilera');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Entre Ríos','Caseros');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Entre Ríos','Villa Urquiza');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Corrientes','Bonpland');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Corrientes','Villa Olivari');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Misiones','Azara');
+INSERT INTO localidades (pais,provincia,ciudad) values ('Argentina','Misiones','Santo Pipó');
+
+/*Se crean los codigos de area; Estan en el mismo orden que las localidades*/
+INSERT INTO codigos_area (codigo) values (3329);
+INSERT INTO codigos_area (codigo) values (3489);
+INSERT INTO codigos_area (codigo) values (2352);
+INSERT INTO codigos_area (codigo) values (2245);
+INSERT INTO codigos_area (codigo) values (11);
+INSERT INTO codigos_area (codigo) values (3487);
+INSERT INTO codigos_area (codigo) values (3491);
+INSERT INTO codigos_area (codigo) values (3496);
+INSERT INTO codigos_area (codigo) values (3482);
+INSERT INTO codigos_area (codigo) values (3476);
+INSERT INTO codigos_area (codigo) values (2925);
+INSERT INTO codigos_area (codigo) values (2335);
+INSERT INTO codigos_area (codigo) values (2302);
+INSERT INTO codigos_area (codigo) values (343);
+INSERT INTO codigos_area (codigo) values (3442);
+INSERT INTO codigos_area (codigo) values (343);
+INSERT INTO codigos_area (codigo) values (3772);
+INSERT INTO codigos_area (codigo) values (3786);
+INSERT INTO codigos_area (codigo) values (3758);
+INSERT INTO codigos_area (codigo) values (376);
+
+/*Prefijo de argentina*/
+INSERT INTO prefijos_internacionales (prefijo) values (54);
