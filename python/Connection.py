e079b463d49ef5361c4b5652453391dac394d719
unknown <guilletejera@gmail.com>
2021-11-15 08:41:06 -0300

    Clase Connection de python

---
+++ python/Connection.py
@@ -0,0 +1,29 @@
+import mariadb
+import sys
+import configdb
+#Crear archivo configdb con las siguientes constantes(cada una con los valores correspondientes a tu base de datos):
+#USER='root'
+#PASS=''
+#DATABASE='laboratorio'
+#HOST='localhost'
+#PORT='3307'
+
+class Connection:
+    __database = None
+    
+    def getConnection():
+        if(Connection.__database == None):
+            try:
+                Connection.__database = mariadb.connect(
+                    user=configdb.USER,
+                    password=configdb.PASS,
+                    host=configdb.HOST,
+                    port=configdb.PORT,
+                    database=configdb.DATABASE
+                )
+            except mariadb.Error as e:
+                print(f"Error al conectar con la base de datos: {e}")
+                sys.exit(1)
+                
+        return Connection.__database.cursor()
+    
