504c9db0d5cb00409217123fcc9afa2d8606d334
unknown <jonitole264@gmail.com>
2021-11-22 19:43:08 -0300

    Creacion archivos access. Falta todavia vincular con los datos de la base de datos

--- python/generador_mi_acc/ScriptAccess.py
+++ python/generador_mi_acc/ScriptAccess.py
@@ -1,38 +1,39 @@
 import msaccessdb
 import pyodbc
-import mysql.connector
-
-"Conexion con la base de datos de los numeros aleatorios"
-"""conexion1 = mysql.connector.connect (host = "localhost", user = "root", passwd = "", database = "python")
-
-cursor1 = conexion1.cursor()"""
+from models.Connection import Connection
 
 
 "Creacion y conexion del archivo Microsoft Access"
 
 
-msaccessdb.create(r'D:\Escritorio\archivo1.accdb')
-conn_str = (
-    r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-    r'DBQ=D:\Escritorio\archivo1.accdb;'
-    )
-cnxn = pyodbc.connect(conn_str)
-crsr = cnxn.cursor()
+i=0
+while (i<10):
+    msaccessdb.create(r'D:\Escritorio\archivo'+ str(i) +'.accdb')
+    conn_str = (
+        r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=D:\Escritorio\archivo'+str(i)+'.accdb;'
+        )
+    cnxn = pyodbc.connect(conn_str)
+    crsr = cnxn.cursor()
 
 
-"Creacion de la tabla numeros"
+    "Creacion de la tabla numeros"
 
 
-crsr.execute("CREATE TABLE numeros (caracteristica VARCHAR(8),numero VARCHAR(20))")
+    crsr.execute("CREATE TABLE numeros (caracteristica VARCHAR(8),numero VARCHAR(20))")
 
 
-"Insercion de numeros en la tabla"
+    "Insercion de numeros en la tabla"
 
 
-crsr.execute("INSERT INTO numeros (caracteristica, numero) VALUES (2325,680723)")
+    crsr.execute("INSERT INTO numeros (caracteristica, numero) VALUES (2325,680723)")
 
 
-"Commit y cerrar la conexion"
-crsr.commit()
-crsr.close()
-cnxn.close()
+    "Commit y cerrar la conexion"
+    crsr.commit()
+    crsr.close()
+    cnxn.close()
+
+    i=i+1
+    
+
