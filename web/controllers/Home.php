2c4f18bc0aca04b4afa8cc3f4d85bd5296d252f0
Pao <paosantii@gmail.com>
2021-11-22 16:39:37 -0300

    nuevasVistas

--- web/controllers/Home.php
+++ web/controllers/Home.php
@@ -12,7 +12,19 @@ abstract class Home {
         echo file_get_contents('src/pages/nuevaCampania/nuevaCampania.html');
     }
 
-    public static function campania():void {
-        echo file_get_contents('src/pages/campania/campania.html');
+    public static function editarCampania():void {
+        echo file_get_contents('src/pages/editarCampania/editarCampania.html');
+    }
+
+    public static function nuevoCliente(): void{
+        echo file_get_contents('src/pages/nuevoCliente/nuevoCliente.html');
+    }
+
+    public static function clientes(): void{
+        echo file_get_contents('src/pages/clientes/clientes.html');
+    }
+
+    public static function editarCliente(): void{
+        echo file_get_contents('src/pages/editarCliente/editarCliente.html');
     }
 }
