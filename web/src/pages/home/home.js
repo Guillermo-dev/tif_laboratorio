6e75850a84edc81a5b97a4d4e1eb09b74388ee20
jorrmaglione <jorr.hs@hotmail.com>
2021-11-25 00:05:25 -0300

    Mejoras front

---
+++ web/src/pages/home/home.js
@@ -0,0 +1,120 @@
+import '/node_modules/izitoast/src/js/iziToast.js';
+import '/node_modules/sweetalert2/dist/sweetalert2.min.js';
+
+const campaniasLista = document.getElementById('CampaniasLista');
+
+const campaniaTemplate = document.getElementById('CampaniaTemplate');
+
+const moreButton = document.getElementById('MoreButton');
+
+const buscadorForm = document.getElementById('Buscador');
+
+buscadorForm.onsubmit = function () {
+    try {
+        fetchCampanias();
+    } catch (error) {
+        window.iziToast.error({message: error.toString()});
+    }
+    return false;
+}
+
+moreButton.onclick = procesarCampanias;
+
+let offset = 2;
+let bufferPointer = 0;
+let campaniasBuffer = [];
+
+function fetchCampanias() {
+    let query = '';
+
+    if (buscadorForm['search'].value.length > 0) {
+        query = `?search=${buscadorForm['search'].value}`
+    }
+
+    fetch(`/src/campanias.json${query}`)
+        .then(httpResp => httpResp.json())
+        .then(response => {
+            if (response.status === 'success') {
+                campaniasBuffer = response.data.campanias;
+
+                campaniasLista.innerHTML = "";
+
+                bufferPointer = 0;
+
+                procesarCampanias();
+            } else {
+                offset.iziToast.error({message: 'Error al obtener las campañas'});
+            }
+        })
+        .catch(reason => {
+            offset.iziToast.error({message: reason.toString()});
+        });
+}
+
+function createCampania(campania) {
+    const element = campaniaTemplate.content.firstElementChild.cloneNode(true);
+
+    const data = element.querySelectorAll('[data-js="data"]');
+
+    data[0].textContent = campania.nombre;
+
+    data[1].textContent = campania.fecha_inicio;
+
+    data[2].textContent = campania.cantidad_mensajes;
+
+    data[3].textContent = campania.estado;
+
+    data[4].textContent = campania.texto_SMS;
+
+    const buttons = element.querySelectorAll('[data-js="button"]');
+
+    buttons[0].onclick = function () {
+        location.href =
+            `/editarCampania/${campania.campania_id}`
+        ;
+    }
+
+    buttons[1].onclick = function () {
+        window.Sweetalert2.fire({
+            icon: 'question',
+            title: '¿Eliminar campaña?',
+            html: 'Estas seguro que deseas eliminar la campaña?',
+            showCancelButton: true,
+            reverseButtons: true,
+        }).then(result => {
+            if (result.isConfirmed) {
+                fetch(
+                    `/api/campanias/${campania.campania_id}`
+                    , {
+                        method: 'DELETE',
+                    })
+                    .then(httpResp => httpResp.json())
+                    .then(response => {
+                        if (response.status === 'success') {
+                            offset.iziToast.success({message: 'La campaña se eliminó con éxito!'});
+                            element.remove();
+                        } else {
+                            offset.iziToast.error({message: 'Error al eliminar la campaña'});
+                        }
+                    })
+                    .catch(reason => {
+                        offset.iziToast.error({message: reason.toString()});
+                    });
+            }
+        });
+    }
+
+    return element
+}
+
+function procesarCampanias() {
+    let to = bufferPointer + offset;
+    for (let i = bufferPointer; i < to && i < campaniasBuffer.length; i++, bufferPointer++) {
+        campaniasLista.append(createCampania(campaniasBuffer[i]));
+    }
+    if (bufferPointer >= campaniasBuffer.length) {
+        moreButton.classList.add('d-none');
+    }
+}
+
+fetchCampanias();
---
+++ web/src/pages/nuevaCampania/nuevaCampania.js
@@ -0,0 +1,114 @@
+import '/node_modules/izitoast/dist/js/iziToast.min.js';
+import '/node_modules/sweetalert2/dist/sweetalert2.min.js';
+
+let cliente = null;
+
+/************************************************/
+
+const clienteForm = document.getElementById('ClienteForm');
+
+clienteForm.onsubmit = function (event) {
+    try {
+        cliente = null;
+
+        if (clienteForm['cuil_cuit'].value.length === 0) {
+            window.iziToast.warning({message: 'Debe ingresar un cuil/cuit'});
+
+            clienteForm.classList.remove('cliente-info');
+
+            clienteForm.reset();
+
+            return false;
+        }
+
+        fetch(`/src/clientes.json?cuil_cuit=${clienteForm['cuil_cuit'].value}`)
+            .then(httpResp => httpResp.json())
+            .then(response => {
+                if (response.status === "success") {
+                    if (response.data.cliente !== null) {
+                        clienteForm.classList.add('cliente-info');
+
+                        procesarCliente(response.data.cliente);
+
+                        cliente = response.data.cliente;
+
+                    } else {
+                        let cuil = clienteForm['cuil_cuit'].value;
+
+                        clienteForm.classList.remove('cliente-info');
+
+                        clienteForm.reset();
+
+                        clienteForm['cuil_cuit'].value = cuil;
+
+                        Sweetalert2.fire({
+                            icon: 'question',
+                            title: 'El cliente no existe',
+                            html: 'Desea crearlo?',
+                            showCancelButton: true,
+                            reverseButtons: true
+                        }).then(result => {
+                            if (result.isConfirmed) {
+                                location.href = '/nuevoCliente'
+                            }
+                        });
+                    }
+                } else {
+                    window.iziToast.error({message: response.error});
+                }
+            })
+            .catch(reason => {
+                window.iziToast.error({message: reason.toString()});
+            });
+    } catch (error) {
+        window.iziToast.error({message: error.toString()});
+    }
+    return false;
+}
+
+function procesarCliente(cliente) {
+    clienteForm['nombre'].value = cliente.nombre;
+
+    clienteForm['apellido'].value = cliente.apellido;
+}
+
+/************************************************/
+
+const campaniaForm = document.getElementById('CampaniaForm');
+
+campaniaForm.onsubmit = function (event) {
+    try {
+        if (!cliente) {
+            window.iziToast.warning({message: 'Debe seleccionar un cliente'});
+            return false;
+        }
+
+        const data = {
+            nombre: campaniaForm['nombre'].value,
+            cantidadMensajes: campaniaForm['cantidad_mensajes'].value,
+            textSMS: campaniaForm['text_SMS'].value,
+            fechaInicio: campaniaForm['fecha_inicio'].value,
+            clienteId: cliente.cliente_id,
+        }
+
+        fetch(`/api/campanias`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(httpResp => httpResp.json()).then(response => {
+            if (response.status === 'success') {
+                window.iziToast.success({message: 'El cliente se creo con exito'});
+                campaniaForm.reset();
+            } else {
+                window.iziToast.error({message: response.error});
+            }
+        }).catch(reason => {
+            window.iziToast.error({message: reason.toString()});
+        });
+    } catch (error) {
+        window.iziToast.error({message: error.toString()});
+    }
+    return false;
+}
